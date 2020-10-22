b460b4414439682194ff7f4f383e62bb24240427
nhanit0511 <72955061+nhanit0511@users.noreply.github.com>
2020-10-22 18:06:57 +0700

    Admin - dashboard

--- user/admin/urls.py
+++ user/admin/urls.py
@@ -6,7 +6,9 @@ from user.admin.views import (
     UserView,
     UserDetailsView,
     BlockUser,
-    UnblockUser
+    UnblockUser,
+
+    Dashboard
 )
 
 urlpatterns = [
@@ -14,4 +16,6 @@ urlpatterns = [
     url(r'^(?P<user_id>[0-9A-Fa-f-]+)/$', UserDetailsView.as_view(), name='detail-user'),
     url(r'^(?P<user_id>[0-9A-Fa-f-]+)/block/$', BlockUser.as_view(), name='block-user'),
     url(r'^(?P<user_id>[0-9A-Fa-f-]+)/unblock/$', UnblockUser.as_view(), name='unblock-user'),
+
+    url(r'^dashboard/$', Dashboard.as_view(), name='unblock-user'),
 ]
